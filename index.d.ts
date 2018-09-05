d3526e4219ca4e6082f2f46296194faf6e5b86d7
Spencer Tuft <spencer.tuft@gmail.com>
2018-09-05 12:34:35 -0600

    Add support for promise paradigm for request function without callback

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,7 @@ declare module 'byu-wso2-request' {
     export function actingForHeader(requestObject: RequestPromiseOptions, actingForNetId: string): void;
     export function request(settings: RequestPromiseOptions): Promise<any>;
     export function request(settings: RequestPromiseOptions, callback: () => any): any;
+    export function request(settings: RequestPromiseOptions, originalJWT: string): Promise<any>;
     export function request(settings: RequestPromiseOptions, originalJWT: string, callback: () => any): any;
 
     export interface OauthSettings {
