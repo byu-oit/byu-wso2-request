d0b5a1cb9dd646265099c045777eadc69133ef84
garygsc <garygsc@gmail.com>
2019-06-11 13:21:34 -0600

    Rewrite request function
    Simplify and use newer version of byu-wabs-oauth
    Node 6 support was already dropped, so this uses async/await shamelessly
    It also drop support for callbacks
    
    Since byu-wabs-oauth's constructor is now async, setOauthSettings is too

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,9 @@
-import {RequestPromiseOptions} from 'request-promise'
+import { RequestPromiseOptions } from 'request-promise'
 
 declare module 'byu-wso2-request' {
-    export function setOauthSettings(settings: OauthSettings): void;
-    export function oauthHttpHeaderValue(token: string): string;
-    export function actingForHeader(requestObject: RequestPromiseOptions, actingForNetId: string): void;
-    export function request(settings: RequestPromiseOptions): Promise<any>;
-    export function request(settings: RequestPromiseOptions, callback: () => any): any;
-    export function request(settings: RequestPromiseOptions, originalJWT: string): Promise<any>;
-    export function request(settings: RequestPromiseOptions, originalJWT: string, callback: () => any): any;
-
-    export interface OauthSettings {
-        clientKey: string,
-        clientSecret: string,
-        wellKnownUrl: string
-    }
+    export function setOauthSettings (clientKey: string, clientSecret: string): Promise<any>
+    export function oauthHttpHeaderValue (token: string): string
+    export function actingForHeader (requestObject: RequestPromiseOptions, actingForNetId: string): void
+    export function request (settings: RequestPromiseOptions): Promise<any>
+    export function request (settings: RequestPromiseOptions, originalJWT: string): Promise<any>
 }
