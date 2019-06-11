63ca1265f904ecdd39ea38d4cc014b2ed237f4a6
garygsc <garygsc@gmail.com>
2019-06-11 16:08:23 -0600

    Remove another test claiming to use a callback

--- tests/wso2request.js
+++ tests/wso2request.js
@@ -27,25 +27,6 @@ describe('wso2request', function () {
     expect(response.body.value.identifying.programId.value).to.equal('32977')
   })
 
-  it('can get api with callback', async function () {
-    let requestObject = {
-      url: 'https://api.byu.edu/curriculum/v1.0/academicProgram/32977',
-      method: 'GET',
-      json: true,
-      resolveWithFullResponse: true,
-      simple: true,
-      encoding: 'utf8',
-      headers: {
-        Accept: 'application/json'
-      }
-    }
-
-    const response = await wso2Request(requestObject)
-    expect(response.body).to.be.an('object')
-    expect(response.statusCode).to.equal(200)
-    expect(response.body.value.identifying.programId.value).to.equal('32977')
-  })
-
   it('can get api with original jwt', async function () {
     let requestObject = {
       url: 'https://api.byu.edu/curriculum/v1.0/academicProgram/32977',
