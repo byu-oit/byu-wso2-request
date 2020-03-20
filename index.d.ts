cc89577fa43a04fad113ec7a94dad9adba3137ac
GaryGSC <GaryGSC@users.noreply.github.com>
2020-03-20 10:36:12 -0600

    Update types for setOauthSettings
    Refs: e20ffb3b32cab665ba9f0da7dee3cd5d391c7b1d

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ import { RequestPromiseOptions } from 'request-promise'
 
 declare module 'byu-wso2-request' {
     export function setOauthSettings (clientKey: string, clientSecret: string): Promise<any>
+    export function setOauthSettings (oauthSettings: { clientKey: string, clientSecret: string }): Promise<any>
     export function oauthHttpHeaderValue (token: string): string
     export function actingForHeader (requestObject: RequestPromiseOptions, actingForNetId: string): void
     export function request (settings: RequestPromiseOptions): Promise<any>
