79c0d6c7c9f1aea7903971dac7383ec96a641dad
Spencer Tuft <stuft2@byu.edu>
2022-06-15 08:56:54 -0600

    tests: add integration test

---
+++ test/integration.js
@@ -0,0 +1,24 @@
+/* global describe before it */
+/* eslint-disable no-unused-expressions */
+const expect = require('chai').expect
+const wso2 = require('../index')
+
+// Do not run these tests in the release pipeline
+describe.skip('Integration Tests', () => {
+  const provider = new URL(process.env.HOST)
+  const clientKey = process.env.CLIENT_KEY
+  const clientSecret = process.env.CLIENT_SECRET
+
+  const resolveWithFullResponse = true // will return request metadata
+  const simple = false // will not throw responses with status codes >399
+
+  before(async () => {
+    await wso2.setOauthSettings(clientKey, clientSecret, { ...provider && { host: provider.host } })
+  })
+
+  it('should get a successful response from echo', async () => {
+    const url = new URL('/echo/v1/echo/test', provider.toString())
+    const response = await wso2.request({ url: url.toString(), simple, resolveWithFullResponse })
+    expect(response.statusCode).equals(200)
+  })
+})
